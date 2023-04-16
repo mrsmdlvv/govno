4107d87831057697f9e248eb6bfc83b964871912
Ghost2021 <rtmuskov@gmail.com>
2023-04-17 01:50:06 +0300

    xui2

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main() {
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
     int X[n];
-    printf("Input the elements:\n");
+    printf("\nInput the elements:\n");
     for (int i = 0; i < n; ++i) {
         scanf("%d", &X[i]);
     }
@@ -98,7 +98,7 @@ int main() {
                 printf("%d ", X[i]);
             }
             for( int i=0; i<n-1;i++){
-                for(int j=0; j<n-i-1;i++){
+                for(int j=0; j<n-i-1;j++){
                     if(X[j]>X[j+1]){
                         g=X[j];
                         X[j]=X[j+1];
